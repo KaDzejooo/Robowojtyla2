169859b424aa56847510e4294752531004f4eab3
Boorgir2137 <117744707+Boorgir2137@users.noreply.github.com>
2024-01-16 17:09:20 +0100

    Added Stepper object and method to use it setAngle(angle)
    wpierdalasz setAngle i jest zajebiście

--- robotControl/inc/Stepper.hpp
+++ robotControl/inc/Stepper.hpp
@@ -9,10 +9,11 @@
 #define STEPPER_HPP_
 
 #include "main.h"
+#include "cmsis_os2.h"
+#include "math.h"
 /*
  *
  */
-
 #define STEPPER_FULL_STEP 1
 #define STEPPER_HALF_STEP 2
 #define STEPPER_QUATER_STEP 4
@@ -21,17 +22,19 @@
 class Stepper
 {
 	public:
-		Stepper(uint8_t stepPin, uint8_t dirPin);
-		void setMicrostepping(uint8_t microstepping);
-		void setStepsPerRev(uint8_t stepsPerRev);
+		Stepper(uint8_t stepPin,uint8_5 stepPort, uint8_t dirPin, uint8_t dirPort);
+		/*void setMicrostepping(uint8_t microstepping);*/
+		/*void setStepsPerRev(uint8_t stepsPerRev);*/
 
 		void setAngle(uint16_t angle);
 
-		virtual ~Stepper( );
-	private:
-		void calcDegrees(uint16_t angle);
 
-		uint8_t _stepsPerRev = 0;
+	private:
+		typedef struct {
+			float deg;
+			int step;}degNStep;
+		degNStep calcDegNStep(float angle);
+		uint8_t _stepsPerRev = 200;
 		uint8_t _microstepping = 1;
 		uint8_t _stepPin,_dirPin;
 };
--- robotControl/src/Stepper.cpp
+++ robotControl/src/Stepper.cpp
@@ -6,15 +6,34 @@
  */
 
 #include <Stepper.hpp>
+Stepper::Stepper(uint8_t stepPin, uint8_5 stepPort, uint8_t dirPin,
+		uint8_t dirPort) {
+	float currentAngle = 0;
 
-Stepper::Stepper( )
-{
-	// TODO Auto-generated constructor stub
-	
 }
 
-Stepper::~Stepper( )
-{
-	// TODO Auto-generated destructor stub
+
+Stepper::setAngle(uint16_t angle) {
+	calcDegNStep(angle);
+	if (realDegNStep.deg > 0)
+		Hal_GPIO_WritePin(dirPort, dirPin, 1);
+	else
+		Hal_GPIO_WritePin(dirPort, dirPin, 0);
+
+	for (int i = 0; i < realDegNStep.step; i++) {
+
+		HAL_GPIO_WritePin(stepPort, stepPin, 1);
+		osDelay(10);
+		HAL_GPIO_WritePin(stepPort, stepPin, 0);
+	}
+
 }
 
+degNStep Stepper::calcDegNStep(float angle) {
+	float toDoAngle = currentAngle - angle;
+	degNStep realDegNStep;
+	float stepF = (toDoAngle / 1.8) - ((toDoAngle / 1.8) % 1);
+	realDegNStep.deg = stepF * 1.8;
+	realDegNStep.step = abs(stepF);
+	return realDegNStep;
+}
